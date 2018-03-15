86ee25af060150e20821b6a582bf30b878caf037
James Pham <vnlegend@gmail.com>
2018-03-15 03:28:16 -0700

    first

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
